faed2d1ae1cf8438c7ed452305fe7d20d273a413
p2212516 <grace.tsoualla-tatidoung@etu.univ-lyon1.fr>
2023-04-19 06:55:35 +0200

    ajout de fichiers de configurations

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sample',
+    version='0.1.0',
+    description='Sample package for Python-Guide.org',
+    long_description=readme,
+    author='Grace Tsoualla',
+    author_email='gracetsoualla@gmail.com',
+    url='https://forge.univ-lyon1.fr/p2212516/projet-servier.git',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
